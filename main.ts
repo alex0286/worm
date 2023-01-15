6268f491ebd596226fe47a0aedd88ad8bd37a04a
alexander mehertab <118993330+alex0286@users.noreply.github.com>
2023-01-14 20:47:07 -0500

    added sound sensor

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
-let meow = 0
 input.onButtonPressed(Button.A, function () {
     meow = 1
+})
+input.onSound(DetectedSound.Loud, function () {
+    sound = input.soundLevel()
+    if (sound > 100) {
+        meow += 1
+        if (meow == 2) {
+            meow = 0
+        }
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     meow = 2
@@ -8,6 +16,10 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     meow = 0
 })
+let sound = 0
+let meow = 0
+meow = 0
+sound = 0
 basic.forever(function () {
     basic.showLeds(`
         . . . . .
